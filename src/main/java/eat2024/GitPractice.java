f23c60049c8f5cfa2f7398911240284438102acc
david <davidsilvaa0147@gmail.com>
2024-05-25 11:51:16 -0400

    added print statement

--- src/main/java/eat2024/GitPractice.java
+++ src/main/java/eat2024/GitPractice.java
@@ -3,5 +3,6 @@ package eat2024;
 public class GitPractice {
     public static void main(String...a){
         System.out.println("I'm into git class");
+        System.out.println("My name is Sah, I am pushing my code to github.");
     }
 }
