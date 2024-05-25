787469dc11e14cbaf7604912f80b2003d7e3aa31
neerajsdet <neerajagarwal090@gmail.com>
2024-05-25 20:50:34 +0530

    Adding basic structure and code

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+V
+&src/main/java/eat2024/GitPractice.java,b\3\b396ac81f1c46d9359fffec352b1a1cdde290994
---
+++ src/main/java/eat2024/GitPractice.java
@@ -0,0 +1,7 @@
+package eat2024;
+
+public class GitPractice {
+    public static void main(String...a){
+        System.out.println("I'm into git class");
+    }
+}
