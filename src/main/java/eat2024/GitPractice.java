cf82e40bc31c6375d60477dfba8930359b95d5be
AnexCipher <hussain.syed.6732@gmail.com>
2024-05-25 12:20:15 -0400

    adding print statement

--- src/main/java/eat2024/GitPractice.java
+++ src/main/java/eat2024/GitPractice.java
@@ -4,5 +4,6 @@ public class GitPractice {
     public static void main(String...a){
         System.out.println("I'm into git class");
         System.out.println("My name is Sah, I am pushing my code to github.");
+        System.out.println("My name is Syed, I am pushing my code to github.");
     }
 }
